4d1ec17a8ccf40a637459d35f5459432dae967e3
abpwrs <alexander-powers@uiowa.edu>
2019-01-28 20:47:57 -0600

    modifying structure

--- scripts/py/reset_config.py
+++ scripts/py/reset_config.py
@@ -1,17 +1,18 @@
 import os
 import json
+
 # All scripts should be run from the project root!
 pwd = os.getcwd()
 config = {
-    "data_dir":str(os.path.join(pwd,"data")),
-    "model_dir":str(os.path.join(pwd,"model")),
-    "out_dir":str(os.path.join(pwd,"out")),
-    "job_dir":str(os.path.join(pwd,"job"))
+    "data_dir": str(os.path.join(pwd, "ml_scraping", "data")),
+    "model_dir": str(os.path.join(pwd, "ml_scraping", "model")),
+    "out_dir": str(os.path.join(pwd, "ml_scraping", "out")),
+    "job_dir": str(os.path.join(pwd, "ml_scraping", "job"))
 }
 
 for d_path in config.values():
     if not os.path.isdir(d_path):
         os.mkdir(d_path)
 
-with open(os.path.join(pwd,"config.json"), "w") as f:
-    json.dump(config,f, indent=4)
+with open(os.path.join(pwd, "config.json"), "w") as f:
+    json.dump(config, f, indent=4)
