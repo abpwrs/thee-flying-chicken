1ef1cde97b67034dc51569d06ea0e81b4249d7c1
Conway, Daniel Y <daniel-conway@uiowa.edu>
2019-03-03 13:46:07 -0600

    Update

--- web/routes/product.route.js
+++ web/routes/product.route.js
@@ -7,4 +7,4 @@ const product_controller = require('../controllers/product.controller');
 
 // a simple test url to check that all of our files are communicating correctly.
 router.get('/test', product_controller.test);
-module.exports = router;
+module.exports = router();
