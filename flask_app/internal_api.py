1d12455ca9af20180bc82cf60412aa19471ebabd
abpwrs <alexander-powers@uiowa.edu>
2019-03-07 18:08:26 -0600

    adding TODOs

--- flask_app/internal_api.py
+++ flask_app/internal_api.py
@@ -1,19 +1,22 @@
 # takes in a url and returns the text content
 def api_scrape(url):
-    # replace this with functionality
+    # TODO: replace this with functionality
     pass
 
 
 # return classification of all named entities in the scraped text
 def api_classify_all(scraped_text):
+    # TODO: replace this with functionality
     pass
 
 
 # return classification for one entity
 def api_classify_entity(scraped_text, entity):
+    # TODO: replace this with functionality
     pass
 
 
 # return the j most common entities, where j is an integer
 def api_classify_top_j(scraped_text, j):
+    # TODO: replace this with functionality
     pass
