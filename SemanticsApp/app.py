6e2863af9dd92b01c6bb4fa3598c7c2b5f898c54
abpwrs <alexander-powers@uiowa.edu>
2019-04-18 16:48:59 -0500

    working on templating heirarchy

--- SemanticsApp/app.py
+++ SemanticsApp/app.py
@@ -1,11 +1,17 @@
 from flask import Flask, redirect, render_template, url_for, request
 
 from database import db_session, init_db
-from models import User
+from models.User import User
 from internal_api import *
 
 app = Flask(__name__)
 init_db()
+
+
+@app.errorhandler(404)
+def page_not_found():
+    # note that we set the 404 status explicitly
+    return render_template('errors/404.html'), 404
 
 
 @app.route('/')
@@ -13,12 +19,12 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route('/hello/<name>')
+@app.route('/hello/<name>/')
 def hello(name):
     return render_template('hello.html', name=name)
 
 
-@app.route('/users/new/<username>/<email>')
+@app.route('/users/new/<username>/<email>/')
 def user_new(username, email):
     new_user = User(name=str(username), email=str(email))
     db_session.add(new_user)
@@ -26,13 +32,13 @@ def user_new(username, email):
     return redirect(url_for('user_index'))
 
 
-@app.route('/users')
+@app.route('/users/')
 def user_index():
     users = User.query.all()
     return render_template('users.html', users=users)
 
 
-@app.route('/users/new', methods=['POST','GET'])
+@app.route('/users/new/', methods=['POST', 'GET'])
 def users_new():
     if request.method == 'POST':
         username = request.form.get('username')
@@ -44,31 +50,69 @@ def users_new():
         users = User.query.all()
         return render_template('users.html', users=users)
 
-@app.route('api/scrape', methods=['POST'])
+
+# Scraping and Classification
+#####################################
+# - Scraping
+# - Classification
+
+
+# Scraping 
+#####################################
+@app.route('/api/scrape/', methods=['POST', 'GET'])
 def scrape():
     if request.method == 'POST':
         url = request.form.get('url')
         pass
+    elif request.method == 'GET':
+        pass
+    else:
+        pass
 
 
-# classification functionality routes
+# Classifications
 #####################################
+# - All
+# - Entity
+# - Top-j
 
-# classify all entities in the text and return as a dictionary
-@app.route('/api/classify/all', methods=['POST', 'GET'])
+# Classify All Named Entities
+#####################################
+@app.route('/api/classify/all', methods=['POST,' 'GET'])
 def classify_all(req_url):
     return api_classify_all(api_scrape(req_url))
 
-# classity a single entity and return a single integer representing the classification
-@app.route('/api/classify/entity/<named_entity>', methods=['POST', 'GET'])
-def classify_entity(req_url, named_entity):
+
+# Single Entity Classification
+#####################################
+@app.route('/api/classify/entity/<named_entity>', methods=['GET'])
+def classify_entity_get(req_url, named_entity):
+    if request.method != 'GET':
+        return page_not_found()
     return api_classify_entity(api_scrape(req_url), named_entity)
 
 
-@app.route('/api/classify/<req_url>/top/<int:j>')
-def classify_top_j(req_url, j):
+@app.route('/api/classify/entity/', methods=['POST'])
+def classify_entity_post(req_url, named_entity):
+    if request.method != 'POST':
+        return page_not_found()
+    return api_classify_entity(api_scrape(req_url), named_entity)
+
+
+# Top-J Classification
+#####################################
+@app.route('/api/classify/<req_url>/top/<int:j>', methods=['GET'])
+def classify_top_j_get(req_url, j):
+    if request.method != 'GET':
+        return page_not_found()
     return api_classify_top_j(api_scrape(req_url), j)
 
+
+@app.route('/api/classify/top/', methods=['POST'])
+def classify_top_j_post(req_url, j):
+    if request.method != 'POST':
+        return page_not_found()
+    return api_classify_top_j(api_scrape(req_url), j)
 
 
 @app.teardown_appcontext
